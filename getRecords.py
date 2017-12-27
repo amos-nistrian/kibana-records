0368f5176624ad37b0edfba5d141d1fd86ea1a70
amosnistrian <amosnistrian@gmail.com>
2017-12-26 17:02:32 -0800

    gathering device type and make

--- getRecords.py
+++ getRecords.py
@@ -1,1 +1,39 @@
-// Getting records
+# Get records
+import json
+
+with open('dec13.txt', 'r') as f:
+    recordOfTheDay = json.load(f)
+
+arrayOfAllPhoneEventsForTheDay = []
+
+for i in range(len(recordOfTheDay)):	
+	# make a phone event array
+	phoneEvent = []
+	print(i+1)
+
+	sourceDict = recordOfTheDay[i]["_source"]
+
+	# getting device type
+	if 'device_type' in sourceDict:
+  		print(sourceDict["device_type"])
+  		phoneEvent.append(sourceDict["device_type"])
+	else:
+  		print ("NO DEVICE_TYPE FOUND, adding Android instead")
+  		phoneEvent.append("Android")
+	
+	# getting make
+	if 'make' in sourceDict:
+		phoneEvent.append(sourceDict["make"])
+	else:
+		print("make was unknown")
+		phoneEvent.append("unknown")
+
+	
+
+
+
+
+
+
+
+
