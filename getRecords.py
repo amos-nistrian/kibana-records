cd19c047dec19ad774d49e561344a69d73b615d4
amosnistrian <amosnistrian@gmail.com>
2017-12-27 15:26:53 -0800

    got time seprated in date and time

--- getRecords.py
+++ getRecords.py
@@ -1,14 +1,16 @@
 # Get records
 import json
+import time
 
-with open('dec13.txt', 'r') as f:
-    recordOfTheDay = json.load(f)
-
+date = "dec13"
 arrayOfAllPhoneEventsForTheDay = []
+
+with open( date + 'PhoneEventsWithoutLocation.txt', 'r') as f:
+    recordOfTheDay = json.load(f)
 
 for i in range(len(recordOfTheDay)):	
 	# make a phone event array
-	phoneEvent = []
+	phoneEvent = ['f']
 
 	sourceDict = recordOfTheDay[i]["_source"]
 
@@ -24,8 +26,18 @@ for i in range(len(recordOfTheDay)):
 	else:
 		phoneEvent.append("unknown")
 
-	# getting recieved time
-	phoneEvent.append(sourceDict["received_at"])
+	# getting recieved time, split it up
+	incorrectTime = sourceDict["received_at"]
+	 
+	# date, dont include the t
+	date = incorrectTime[0:10]
+
+	# dont include the z
+	time = incorrectTime[11:len(incorrectTime)-2]	# should be 11:12 
+
+	phoneEvent.append(date)
+
+	phoneEvent.append(time)
 
 	# getting imei
 	if 'imei' in sourceDict:
@@ -38,48 +50,77 @@ for i in range(len(recordOfTheDay)):
   	#message = sourceDict["message"]
 
 	if "sap=" not in message:  
-  		phoneEvent.append(0)
+  		phoneEvent.append('0')
 	else:
 		startIndex = (message.index("sap="))
 		startIndex += 3
-		#print(message[startIndex])
+
 		sapVal = "" 
-		for i in range (4):
-			sapVal += message[i+1+startIndex]
-			phoneEvent.append(sapVal)
+		for j in range (4):
+			sapVal += message[j+1+startIndex]
+		
+		phoneEvent.append(sapVal.upper())
+	#print("the sapval at", i+1, "is", sapVal)
 		
 	# getting coupler_id
 	if 'coupler_id' not in sourceDict:	
-		phoneEvent.append(0)
+		phoneEvent.append('0')
 	else:
 		phoneEvent.append(sourceDict["coupler_id"])
 
-	# get city and state
-	# export as csv
+	# add the phoneEvent to arrayOfAllPhoneEventsForTheDay
+	arrayOfAllPhoneEventsForTheDay.append(phoneEvent)
+
+# append city and state
 with open('locations.txt', 'r') as f:
     allLocationRecords = json.load(f)
-    print(len(allLocationRecords))
+    #print(len(allLocationRecords))
 
+# go through locations file
 for i in range(len(allLocationRecords)):
 	# skip the first
 	if i == 0:
 		continue
 	else:
-		print(i)
 		# get the city 
 		city = allLocationRecords[i]['FIELD13']
 
 		# get the state
 		state = allLocationRecords[i]['FIELD14']
-
+	
 		# get the sap
 		sap = allLocationRecords[i]['FIELD4']
 
 		# if sap is only 3 digits prepend 0
 		if (len(allLocationRecords[i]['FIELD4']) < 4):
 			sap = '0' + sap
-			#print("record", i+1)
-			print("PREPENDINGGGGGGGGGGGGGGGGGGGGGGGGGGG")
-			print(sap)
+		
+		# find the matching saps in arrayOfAllPhoneEventsForTheDay and 
+		if sap != 0:
+			# append city and state to all of them if city and state are empty
+			for i in range(len(arrayOfAllPhoneEventsForTheDay)):
+				if arrayOfAllPhoneEventsForTheDay[i][6] == sap and arrayOfAllPhoneEventsForTheDay[i][0] == 'f':
+					arrayOfAllPhoneEventsForTheDay[i].append(city)
+					arrayOfAllPhoneEventsForTheDay[i].append(state)
+					arrayOfAllPhoneEventsForTheDay[i][0] = 't'
 
-		print(sap)
+# export arrayOfAllPhoneEventsForTheDay as csv
+
+# create file to write to
+file = open(date + "PhoneEventsAddedLocation.txt","w+")
+
+for i in range(len(arrayOfAllPhoneEventsForTheDay)):
+	print(i+1)
+	for j in range(len(arrayOfAllPhoneEventsForTheDay[i])):
+		#print (len(arrayOfAllPhoneEventsForTheDay[i]))
+		# get that value and append comma and newline at end
+		if j == 0:
+			continue
+		else:
+			if j+1 == len(arrayOfAllPhoneEventsForTheDay[i]):
+				print("appending", arrayOfAllPhoneEventsForTheDay[i][j] + '\n')
+				file.write(arrayOfAllPhoneEventsForTheDay[i][j] + "\n")
+			else:
+				print("appending", arrayOfAllPhoneEventsForTheDay[i][j] + ',')
+				file.write(arrayOfAllPhoneEventsForTheDay[i][j] + ',')
+
