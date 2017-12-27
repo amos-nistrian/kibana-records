535ade740c29817db3266f4a8e08c7c3bc64ad35
amosnistrian <amosnistrian@gmail.com>
2017-12-26 17:35:37 -0800

    gathered coupler_id

--- getRecords.py
+++ getRecords.py
@@ -18,14 +18,12 @@ for i in range(len(recordOfTheDay)):
   		print(sourceDict["device_type"])
   		phoneEvent.append(sourceDict["device_type"])
 	else:
-  		print ("NO DEVICE_TYPE FOUND, adding Android instead")
   		phoneEvent.append("Android")
 	
 	# getting make
 	if 'make' in sourceDict:
 		phoneEvent.append(sourceDict["make"])
 	else:
-		print("make was unknown")
 		phoneEvent.append("unknown")
 
 	# getting recieved time
@@ -35,10 +33,32 @@ for i in range(len(recordOfTheDay)):
 	if 'imei' in sourceDict:
   		phoneEvent.append("yes")
 	else:
-  		phoneEvent.append("no")
+		print("APPENDONG NO")
+		phoneEvent.append("no")
 
-	
+	# getting sap
+	message = recordOfTheDay[i]["_source"]["message"]
+  	#message = sourceDict["message"]
 
+	if "sap=" not in message:  
+  		phoneEvent.append(0)
+	else:
+		startIndex = (message.index("sap="))
+		startIndex += 3
+		#print(message[startIndex])
+		sapVal = "" 
+		for i in range (4):
+			sapVal += message[i+1+startIndex]
+			print ("sap =", sapVal)
+			phoneEvent.append(sapVal)
+		
+	# getting coupler_id
+	if 'coupler_id' not in sourceDict:	
+		print("COUPLER NOT FOUNDDDDDDDDDDDDD")
+		phoneEvent.append(0)
+	else:
+		print("COUPLER ID IS", sourceDict["coupler_id"])
+		phoneEvent.append(sourceDict["coupler_id"])
 
 
 
